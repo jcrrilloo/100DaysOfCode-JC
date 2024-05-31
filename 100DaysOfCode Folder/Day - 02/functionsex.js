e05a60791e657dc725b661ea849eca04523db065
Jaay Carrillo <8797753+JaayCarrillo@users.noreply.github.com>
2024-05-30 21:23:52 -0700

    Day 2 / 100 Days of Code

---
+++ 100DaysOfCode Folder/Day - 02/functionsex.js
@@ -0,0 +1,48 @@
+// 1.
+function testOne(firstName) {
+    return "First Name: " + firstName;
+}
+// 2. 
+function testTwo(lastName) {
+    return "Last Name: " + lastName;
+}
+// 3. 
+function testThree(birthDay){
+    return "Birthday: " + birthDay;
+}
+// 4. 
+function testFour(birthLocation) {
+    return "Born in: " + birthLocation
+}
+function testFive(age){
+    return "Age: " + age;
+}
+// 5.
+function testSix(zodiacSign){
+    return "Zodiac Sign: " + zodiacSign;
+}
+// 6. 
+function testSeven(favoriteFood){
+    return "Favorite Food: " + favoriteFood;
+}
+
+function testEight(favoriteDrink){
+    return "Favorite Drink: " + favoriteDrink;
+}
+function testNine(gender){
+    return "Gender: " + gender;
+}
+function testTen(height){
+    return "Height: " + height;
+}
+// Test Case: 
+console.log(testOne("Joseph"));
+console.log(testTwo("Carrillo"));
+console.log(testThree("October"));
+console.log(testFour("Anaheim"));
+console.log(testFive(27));
+console.log(testSix("Libra"));
+console.log(testSeven("Tacos"));
+console.log(testEight("Coffee"));
+console.log(testNine("Male"));
+console.log(testTen("5'10"));
