82a975256f4faf4fd5b9a8048a35923f455fd03d
Jaay Carrillo <8797753+JaayCarrillo@users.noreply.github.com>
2024-06-06 00:05:04 -0700

    if else practice problems cont.

---
+++ 100DaysOfCode Folder/Day - 03/evenorodd.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ *  Write a  progrram that checks if a number
+ * is even or odd
+ */
+console.log("Excercise 1: Even or Odd Number");
+console.log("---------------------");
+function evenOdd(num){
+    if(num % 2 === 0){
+        return "Even Number";
+    } else {
+        return "Odd Number";
+    }
+}
+console.log(evenOdd(120));
+console.log(evenOdd(17));
+console.log("---------------------");
+console.log("Excercise 2: ");
